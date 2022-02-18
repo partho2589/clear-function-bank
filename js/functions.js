8f00e9438bdc1701666e93e0adc4ad554ea5dd44
partho2589 <parthomalo615@.com>
2022-02-18 21:08:09 +0600

    shot a function

--- js/functions.js
+++ js/functions.js
@@ -8,16 +8,9 @@ function getInputValu (fieldId){
     return value
 }
 
+
 // update diposit balance
 
-function updateTotal (fieldId, amount){
-    const totalTag = document.getElementById(fieldId);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText);
-    const newTotal = previousTotal + amount;
-    totalTag.innerText = newTotal;
-    return newTotal;
-};
 
 function getInnerTextValue (fieldId){
     const fieldTag = document.getElementById(fieldId);
@@ -26,19 +19,32 @@ function getInnerTextValue (fieldId){
     return  value;
 }
 
+function updateTotal (fieldId, amount){
+   /*  const totalTag = document.getElementById(fieldId);
+    const previousTotalInText = totalTag.innerText;
+    const previousTotal = parseFloat(previousTotalInText); */
+    const previousTotal = getInnerTextValue(fieldId);
+    const newTotal = previousTotal + amount;
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
+};
+
+
 // update balance 
 function updateBalance (amount, isAdding ){
-    const balanceTag = document.getElementById('balance-total');
+    /* const balanceTag = document.getElementById('balance-total');
     const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText);
+    const previousBalance = parseFloat(balanceInText); */
+    const previousBalance = getInnerTextValue('balance-total')
     let newBalance;
     if (isAdding == true){
          newBalance = previousBalance + amount;
+         
     }else{
          newBalance = previousBalance - amount;
     }
     
-    balanceTag.innerText = newBalance;
+    document.getElementById('balance-total').innerText = newBalance;
 }
 
 // handel diposit amount
@@ -60,5 +66,6 @@ document.getElementById('Withdraw-button').addEventListener('click',function(){
    if(amount >0 && amount <= balance ){
     updateTotal('withdraw-total',amount);
     updateBalance(amount, false)
-   }
+    
+   }return alert('You do not have enough money in your account')
 })
