0727f3db7a6afecaf2c5278ea25589590a3eb7ba
partho2589 <parthomalo615@.com>
2022-02-21 01:26:47 +0600

    added footer tag

--- js/players.js
+++
@@ -1,8 +0,0 @@
-const players = document.getElementsByClassName('player');
-for ( const player of players){
-    console.log(player);
-    player.style.border = '1px solid red';
-    player.style.margin = '10px';
-    player.style.borderRadius = '20px';
-    player.style.padding = '20px';
-}
